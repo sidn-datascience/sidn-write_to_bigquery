7ba95c8ffc17e08460390aea1bb0c5f5d1d4c1a0
acosidn <alessandro.cosci@sidn.es>
2024-11-26 16:53:23 +0100

    adding function writeDfToBq_with_merging

--- write_to_bq/main.py
+++ write_to_bq/main.py
@@ -56,3 +56,102 @@ def writeDfToBq(data:pd.DataFrame, project_id:str, dataset_id:str, table_id:str,
     )
     print(f"Load job creado con el siguiente id: {load_job.job_id}.")
     return load_job
+
+def writeDfToBq_with_merging(data:pd.DataFrame, project_id:str, dataset_id:str, table_id:str, job_id_prefix:str) -> None:
+    """Sends a pandas DataFrame to a BigQuery table with automatic schema handling and data merging.
+
+    Args:
+        data (pd.DataFrame): The pandas DataFrame to be loaded into BigQuery.
+        project_id (str): The ID of your GCP project where the BigQuery dataset resides.
+        dataset_id (str): The ID of the BigQuery dataset containing the target table.
+        table_id (str): The ID of the BigQuery table to be loaded with the data.
+        job_id_prefix (str): A prefix for job IDs to avoid naming conflicts.
+
+    Raises:
+        Exception: An exception with details on errors encountered during the process.
+    """
+    print("Creating the Bigquery client")
+    bq_client = bigquery.Client(project=project_id)
+    table = bigquery.Table(f"{project_id}.{dataset_id}.{table_id}")
+    temp_table = bigquery.Table(f"{project_id}.{dataset_id}.{table_id}_temptable")
+    job_config = bigquery.LoadJobConfig()
+    job_config.source_format = bigquery.SourceFormat.PARQUET
+    job_config.create_disposition = bigquery.CreateDisposition.CREATE_IF_NEEDED
+    job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
+
+    # Schema handling (automatic or based on existing table)
+    try:
+        # Attempt to get the existing production table schema
+        table_schema = bq_client.get_table(table).schema 
+        
+        # Remove fields from the schema that are not present in the DataFrame
+        fields_to_remove = [field for field in table_schema if field.name not in data.columns]
+        for field in fields_to_remove:
+            table_schema.remove(field)
+        job_config.schema = table_schema
+        job_config.autodetect = False # Use the provided schema
+    except:
+        job_config.autodetect = True
+        print('No se ha podido recuperar el esquema de la tabla. Es posible que la tabla no exista.')
+
+    # Create the load job
+    load_job = bq_client.load_table_from_dataframe(
+        dataframe = data,
+        destination = temp_table,
+        job_id = f"{job_id_prefix}_temptable_{pd.to_datetime('now').strftime('%Y%m%d%H%M%S')}",
+        job_config = job_config,
+        project = project_id
+    )
+    print(f"Load job creado con el siguiente id: {load_job.job_id}.")
+
+    # Loading data as a temp table
+    try:
+        output = load_job.result()
+        print(f"Load job {load_job.job_id} ejecutado. Output: {output}.")
+    except Exception as e:
+        raise Exception(f"Se han detectado {len(load_job.errors)} errores durante la ejecución de {load_job.job_id}:\n{[err for err in load_job.errors]}")
+
+    # Merging the temp table into the target table
+    dimensions = data.select_dtypes(exclude=['float','int']).columns
+    metrics = data.select_dtypes(include=['float','int']).columns
+
+    NL = '\n' # new line for f-strings
+    merge_query = f"""
+    BEGIN
+        BEGIN TRANSACTION;
+            MERGE INTO `{project_id}.{dataset_id}.{table_id}` AS target
+            USING `{project_id}.{dataset_id}.{table_id}_temptable` AS source
+            ON {f'{NL}AND '.join([f'target.{dimension} = source.{dimension}' for dimension in dimensions])}
+            WHEN MATCHED THEN
+            UPDATE SET
+                {f',{NL}'.join([f'target.{metric} = source.{metric}' for metric in metrics])}
+            WHEN NOT MATCHED THEN
+                INSERT ({', '.join([*dimensions,*metrics])})
+                VALUES ({', '.join([f'source.{element}' for element in [*dimensions,*metrics]])});
+
+        COMMIT TRANSACTION;
+        
+        EXCEPTION WHEN ERROR THEN
+            SELECT @@error.message;
+            ROLLBACK TRANSACTION;
+    END;"""
+    
+    merge_job = bq_client.query(query=merge_query, job_id=f"{job_id_prefix}_merge_data_{pd.to_datetime('now').strftime('%Y%m%d%H%M%S')}")
+    try:
+        merge_output = merge_job.result()
+        print(f"Merge job {merge_job.job_id} executed. Output: {merge_output}.")
+    except Exception as e:
+        raise Exception(f"Se han detectado {len(merge_job.errors)} errores durante la ejecución de {merge_job.job_id}:\n{[err for err in merge_job.errors]}")
+    
+    # Delete the temptable after a correct merging
+    delete_job = bq_client.query(
+        query=f"DROP TABLE `{project_id}.{dataset_id}.{table_id}_temptable`;",
+        job_id=f"{job_id_prefix}_delete_temp_data_{pd.to_datetime('now').strftime('%Y%m%d%H%M%S')}"
+    )
+    try:
+        delete_output = delete_job.result()
+        print(f"Merge job {delete_job.job_id} executed. Output: {delete_output}.")
+    except Exception as e:
+        raise Exception(f"Se han detectado {len(delete_job.errors)} errores durante la ejecución de {delete_job.job_id}:\n{[err for err in delete_job.errors]}")
+    
+    return load_job, merge_job, delete_job
