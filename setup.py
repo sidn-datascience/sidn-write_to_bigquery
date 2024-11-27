e29877f785f94396a62e49aac383c7e3b2f982d1
acosidn <alessandro.cosci@sidn.es>
2024-11-27 09:45:48 +0100

    update lib version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'write_to_bq',
-    version = '1.0.0',
+    version = '1.1.0',
     packages = find_packages(),
     description = "Sends Pandas DataFrame content to a Bigquery table",
     python_requires=">=3.9",
